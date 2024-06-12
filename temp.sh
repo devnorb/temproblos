0bfe67c6b90f9f5590e01dfc7e1f441d7c7638da
norb_ <127006485+devnorb@users.noreply.github.com>
2024-06-12 16:55:36 -0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -58,16 +58,15 @@ main() {
 
     echo -n "Updating Dylib..."
     curl -Os "https://git.abyssdigital.xyz/preview/macsploit.dylib"
-    curl -Os "https://raw.githubusercontent.com/devnorb/temproblos/main/libNoUpdate.dylib"
 
     echo -e " Done."
     echo -e "Patching Roblox..."
     mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-    mv ./libNoUpdate.dylib "/Applications/Roblox.app/Contents/MacOS/libUpdateDisabler.dylib"
+    # mv ./libNoUpdate.dylib "/Applications/Roblox.app/Contents/MacOS/libUpdateDisabler.dylib"
     mv ./libdiscord-rpc.dylib "/Applications/Roblox.app/Contents/MacOS/libdiscord-rpc.dylib"
     ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
-    echo -e " !!! PRESS n !!! "
-    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libUpdateDisabler.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig 
+    # echo -e " !!! PRESS n !!! "
+    # ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libUpdateDisabler.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig 
     mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
     rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
     rm ./insert_dylib
@@ -77,7 +76,6 @@ main() {
     mv ./MacSploit.app /Applications/MacSploit.app
     rm ./MacSploit.zip
     echo -e "Done."
-
     echo -e "Install Complete! Developed by Nexus42 (norb was here)!"
     exit
 }
